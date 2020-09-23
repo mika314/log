c9a7489d1c08002def2e37e7930a18bbeb88854c
Mika Pi <mika@mika.global>
2020-09-22 19:32:32 -0700

    Use fold expression

--- log.hpp
+++ log.hpp
@@ -3,20 +3,12 @@
 
 namespace Internal
 {
-  inline void logToStrm(std::ostream &strm) { strm << std::endl; }
-
-  template <typename Arg, typename... Args>
-  void logToStrm(std::ostream &strm, Arg &&arg, Args &&... args)
-  {
-    strm << " " << arg;
-    logToStrm(strm, args...);
-  }
-
   template <typename... Args>
   void log(const char *fileName, int line, Args &&... args)
   {
     std::cout << fileName << ":" << line << ":";
-    logToStrm(std::cout, args...);
+    ((std::cout << " " << std::forward<Args>(args)), ...);
+    std::cout << std::endl;
   }
 } // namespace Internal
 
